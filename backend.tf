79a2030bed901c9e4fd6328d811bd03f5e9c646d
sateesh <sateesh0584@gmail.com>
2024-02-21 13:11:44 +0530

    updated

---
+++ backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "sateesh0584"
+    region         = "ap-south-1"
+    key            = "DevOps Project-11/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
