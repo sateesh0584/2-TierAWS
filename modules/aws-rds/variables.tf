047d5c3b54ecf2f96bba2cf19f4e7cd6391f17a5
sateesh0584 <143077082+sateesh0584@users.noreply.github.com>
2024-04-23 20:32:02 +0530

    Update variables.tf

--- modules/aws-rds/variables.tf
+++ modules/aws-rds/variables.tf
@@ -2,7 +2,7 @@ variable "sg-name" {}
 variable "private-subnet-name1" {}
 variable "private-subnet-name2" {}
 variable "db-sg-name" {}
-variable "rds-username" {}
-variable "rds-pwd" {}
+variable "username" {}
+variable "password" {}
 variable "db-name" {}
 variable "rds-name" {}
